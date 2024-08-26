74567f8eb3db57af4edd68500cb381310fe9b5ad
Rex Rony Jacob <83328940+rexxrony@users.noreply.github.com>
2024-08-26 15:10:17 +0530

    Deployed website

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import "@fontsource/roboto"
+import "@fontsource/roboto" 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
