43509fddb1e0f426a211054f2f2bf9019443a04f
Rex Rony Jacob <83328940+rexxrony@users.noreply.github.com>
2026-04-01 22:56:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Rex-Website/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
